fccb5d13146c172ac7ca85b6f9967d1564d8dfba
pedro <pedbcrespo@gmail.com>
2026-09-14 11:40:26 -0300

    formatando codigo

--- scripts.js
+++ scripts.js
@@ -12,33 +12,12 @@ const statusStyle = {
 };
 
 const states = [
-    "AC",
-    "AL",
-    "AP",
-    "AM",
-    "BA",
-    "CE",
-    "DF",
-    "ES",
-    "GO",
-    "MA",
-    "MT",
-    "MS",
-    "MG",
-    "PA",
-    "PB",
-    "PR",
-    "PE",
-    "PI",
-    "RJ",
-    "RN",
-    "RS",
-    "RO",
-    "RR",
-    "SC",
-    "SP",
-    "SE",
-    "TO"
+    "AC", "AL", "AP", "AM", "BA", 
+    "CE", "DF", "ES", "GO", "MA", 
+    "MT", "MS", "MG", "PA", "PB", 
+    "PR", "PE", "PI", "RJ", "RN", 
+    "RS", "RO", "RR", "SC", "SP", 
+    "SE", "TO"
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
